e70cd20678e2e8fc2e6db305d60672fce35c8c7e
neolparcial <nick.lp@hotmail.com>
2018-02-24 14:44:18 -0400

    template admin

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -12,13 +12,13 @@ class Page {
 		"data"=>[]
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir = "/views/", $cache_dir = "/views-cache/"){
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"].$cache_dir,
 			"debug"         => false // set to false to improve the speed
 	   );
 
