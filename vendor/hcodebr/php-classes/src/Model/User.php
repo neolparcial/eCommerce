c4a52c2d23007e584a284c42df67815f5ccc6644
neolparcial <nick.lp@hotmail.com>
2018-02-24 20:51:21 -0400

    crud completo

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -9,59 +9,119 @@ class User extends Model{
 
 	const SESSION = "User";
 
-public static function login($login, $password){
+	public static function login($login, $password){
 
-	$sql = new Sql();
+		$sql = new Sql();
 
-	$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
-		":LOGIN"=>$login
-	));
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+			":LOGIN"=>$login
+		));
 
-	if(count($results) === 0){
+		if(count($results) === 0){
 
-		throw new \Exception("Usuário inexistente ou senha inválida.");		
+			throw new \Exception("Usuário inexistente ou senha inválida.");		
+		}
+
+		$data = $results[0];
+
+		if(password_verify($password, $data["despassword"]) === true){
+
+			$user = new User();
+
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+		}else{
+
+			throw new \Exception("Usuário inexistente ou senha inválida.");
+		}
 	}
 
-	$data = $results[0];
+	public static function verifyLogin($inadmin = true){
 
-	if(password_verify($password, $data["despassword"]) === true){
-
-		$user = new User();
-
-		$user->setData($data);
-
-		$_SESSION[User::SESSION] = $user->getValues();
-
-		return $user;
-
-	}else{
-
-		throw new \Exception("Usuário inexistente ou senha inválida.");
+		if(
+			!isset($_SESSION[User::SESSION])
+			||
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0
+			||
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+		){
+			header("Location: /admin/login");
+			exit;
+		}
 	}
-}
 
-public static function verifyLogin($inadmin = true){
+	public static function logout(){
 
-	if(
-		!isset($_SESSION[User::SESSION])
-		||
-		!$_SESSION[User::SESSION]
-		||
-		!(int)$_SESSION[User::SESSION]["iduser"] > 0
-		||
-		(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-	){
-		header("Location: /admin/login");
-		exit;
+		$_SESSION[User::SESSION] = NULL;
 	}
+
+	public static function listAll(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+	}
+
+	public function save(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, 
+		:inadmin)", array(
+		    ":desperson"=>$this->getdesperson(),
+		    ":deslogin"=>$this->getdeslogin(),
+		    ":despassword"=>$this->getdespassword(),
+		    ":desemail"=>$this->getdesemail(),
+		    ":nrphone"=>$this->getnrphone(),
+		    ":inadmin"=>$this->getinadmin()
+		));
+		$this->setData($results[0]);
+	}
+
+	public function get($iduser){
+
+		$sql = new Sql();
+
+		$result = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+			":iduser"=>$iduser
+		));
+
+		$this->setData($result[0]);
+	}
+
+	public function update(){
+
+		$sql = new Sql();
+
+		$result = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			"desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($result[0]);
+
+	}
+
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>getiduser()
+		));
+	}
+
 }
-
-public static function logout(){
-
-	$_SESSION[User::SESSION] = NULL;
-}
-
-}
-
 
  ?>
