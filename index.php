5a44c2b74ec4f22037298ea0ef104f849e5b05e2
neolparcial <nick.lp@hotmail.com>
2018-03-06 18:24:23 -0400

    Categorias

--- index.php
+++ index.php
@@ -310,6 +310,25 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'pruducts'=>[]
+	]);
+
+});
+
+
+
+
+
 $app->run();
 
 
